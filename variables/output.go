0e40b41a7fc47261601ab85eab8ce393d83f0044
raxmatillo <milodcomposer@gmail.com>
2023-04-09 15:46:37 +0500

    output

---
+++ variables/output.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i := "Hello"
+	j := 15
+	fmt.Printf("i has value: %v and type: %T\n", i, i)
+	fmt.Printf("i has value: %v and type: %T\n", j, j)
+}
